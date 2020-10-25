a12b4067fd84babe83f822db3cbb507458b266f5
Ritesh <riteshtnk11@gmail.com>
2020-10-25 21:06:04 +0700

    [INIT] Initialization of project

---
+++ telegram-react/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Let's build telegram clone</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ telegram-react/src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDDHy5FuaKVxV93eHVRywU_Mfdn0EA0bYI",
+    authDomain: "telegram-react-f0e05.firebaseapp.com",
+    databaseURL: "https://telegram-react-f0e05.firebaseio.com",
+    projectId: "telegram-react-f0e05",
+    storageBucket: "telegram-react-f0e05.appspot.com",
+    messagingSenderId: "1066163731434",
+    appId: "1:1066163731434:web:b742c6d73ad89e0df31093",
+    measurementId: "G-PCRYEVYNHJ"
+  };
