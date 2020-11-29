7037414868b38fc60094c385061695051192a769
riteshtnk11 <riteshtnk11@gmail.com>
2020-11-29 23:37:34 +0530

    [UI] Send message UI

--- telegram-react/src/components/MainChat.js
+++ telegram-react/src/components/MainChat.js
@@ -1,9 +1,22 @@
 import { Avatar, IconButton } from "@material-ui/core";
+import SelectInput from "@material-ui/core/Select/SelectInput";
 import MoreHoriz from "@material-ui/icons/MoreHoriz";
-import React from "react";
+import React, { useState } from "react";
 import "./MainChat.css";
 
 const MainChat = () => {
+  //Declare main state for input field
+  const [input, setInput] = useState('');
+
+  const sendMessage = (e) => {
+    //prevents the default action of submit button.
+    e.preventDefault();
+    //Clears the input field after message is submitted.
+    setInput('');
+  }
+
+  console.log(input);
+
   return (
     <div className="chat">
       <div className="chat__header"></div>
@@ -16,6 +29,13 @@ const MainChat = () => {
         <IconButton>
           <MoreHoriz className="chat__header__details" />
         </IconButton>
+      </div>
+      <div className="chat__messages">
+        <div className="chat__input">
+          <input placeholder="write a message..." type="text" value={input} onChange={(e) => setInput(e.target.value)}></input>
+          <button onClick={ sendMessage }>Send Message</button>
+        </div>
+
       </div>
     </div>
   );
